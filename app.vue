7de0ca4f0a8a77b83fdf8ef90c82d5016edcc6b8
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-08-24 00:39:30 +0700

    add style

--- app.vue
+++ app.vue
@@ -1,11 +1,17 @@
 <template>
-    <div>
+    <div class=" main">
       <NuxtLayout >
           <NuxtPage/>
       </NuxtLayout>
     </div>
 </template>
 <style scoped >
-
+.main {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
 </style>
 

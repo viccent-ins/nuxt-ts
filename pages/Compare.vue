4c34336f920425804207f55213472c97b81d5022
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-08-25 22:39:10 +0700

    call compare function

--- pages/Compare.vue
+++ pages/Compare.vue
@@ -20,50 +20,44 @@
       <div class="  bg-white rounded-sm shadow p-4">
         <div class="flex items-center py-6 justify-between w-full border-b-[1px] ">
           <div class="order-1 ">
-            <p>Comparison</p>
+            <p class=" font-semibold">Comparison</p>
           </div>
           <div class="order-2 block ">
-            <button class="btn bg-red-100 hover:bg-red-500 font-bold px-3 py-2 text-primary hover:text-white rounded-md" >Reset compare list</button>
+            <el-button type="primary" class="" size="large" @click="resetCompare">Reset compare list</el-button>
+<!--            <button class="btn bg-red-100 hover:bg-red-500 font-bold px-3 py-2 text-primary hover:text-white rounded-md" >Reset compare list</button>-->
           </div>
         </div>
         <div class=" my-4">
-          <table class="border-[1px] flex flex-col   justify-between w-full">
-            <thead>
-              <tr class="flex justify-between text-left  items-center border-b-[1px] ">
-                <th class="w-2/5 px-2" >Name</th>
-                <th class="w-3/5 border-l-[2px] p-2">Product Title</th>
-              </tr>
-            </thead>
-            <tbody class="flex flex-col justify-between w-full ">
-              <tr class="flex justify-between text-left  items-center border-b-[1px]">
-                <th class="w-2/5 px-2">Image</th>
-                <td class="w-3/5 p-2 border-l-[2px]"><img class="w-full" src="https://wal-marting.com/public/uploads/all/RJ00gJwWg2NjBQgNJrUjyaf09G2qRZ4xjLPEJeyy.jpg" alt=""></td>
-              </tr>
-              <tr class="flex justify-between text-left  items-center border-b-[1px]">
-                <th class="w-2/5 px-2 ">Price</th>
-                <td class="w-3/5 p-2 primary font-bold  border-l-[2px]">30.96 <span>$</span></td>
-              </tr>
-              <tr class="flex justify-between text-left  items-center border-b-[1px]">
-                <th class="w-2/5 p-2">Brand</th>
-                <td class="w-3/5 p-2 border-l-[2px]"></td>
-              </tr>
-              <tr class="flex justify-between text-left  items-center border-b-[1px]">
-                <th class="w-2/5 px-2">Category</th>
-                <td class="w-3/5 p-2 border-l-[2px]">Home decoration</td>
-              </tr>
-              <tr class="flex justify-between w-full   border-b-[1px]">
-                <th class="w-2/5 "></th>
-                <td class="w-3/5 border-l-[2px] flex justify-center p-2 ">
-                  <button type="button"
-                          class="btn bg-red-200 hover:bg-red-500 flex items-center font-semibold px-4 py-2 rounded-md text-red-500 hover:text-white  text-md mr-2 "
-                          @click="addToCart(product)">
-                    <icon name="material-symbols:shopping-bag-outline-sharp" size="20"></icon>
-                    <span> Add to cart</span>
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div class="flex">
+            <div class="flex flex-col justify-between border ">
+              <div class="border-b py-3">
+                <h5 class="text-lg font-semibold px-2">Name</h5>
+              </div>
+              <div class=" w-60 h-96 py-3">
+                <h5 class="text-lg font-semibold px-2">Image</h5>
+              </div>
+              <div class="border-t h-10">
+                <h5 class="text-lg font-semibold px-2">Price</h5>
+              </div>
+              <div class="border-t h-16">
+                <h5 class="text-lg font-semibold px-2"></h5>
+              </div>
+            </div>
+            <div class="flex flex-col justify-between border justify-center w-full" v-for="(compareProduct , index) in compareProducts" :key="index" >
+              <div class="border-b">
+                <h5 class="py-3 text-lg font-semibold px-2">{{ compareProduct.title }}</h5>
+              </div>
+              <div class="w-full py-3 flex items-center">
+                <img class="w-96 h-96  mx-auto  " :src="compareProduct.image" alt="compareProduct.title">
+              </div>
+              <div class="border-t py-3">
+                <h5 class="text-lg font-semibold primary px-2">{{ compareProduct.price }}$</h5>
+              </div>
+              <div class="border-t py-3 text-center">
+                <el-button type="primary" :icon="Handbag" size="large" @click="addToCart(prop.Product)">Add to cart</el-button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -72,5 +66,23 @@
 </div>
 </template>
 <script setup>
+import {
+  Handbag
+} from '@element-plus/icons-vue'
+import { storeToRefs } from "pinia";
+import { useStores } from "~/store/store";
+import useUserBuyActivity from "~/composables/useUserBuyActivity";
+const {
+  addToCart,
+  compareProduct,
+} = useUserBuyActivity();
 
+const prop = defineProps({
+  Product: {
+    type: Object,
+    required: true
+  }
+});
+const { compareProducts } = storeToRefs(useStores());
+console.log(compareProducts)
 </script>

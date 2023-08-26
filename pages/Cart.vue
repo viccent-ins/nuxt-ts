a54e5547f4799249bdf1de2efdada274c28b8b54
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-08-26 17:35:48 +0700

    remove color

--- pages/Cart.vue
+++ pages/Cart.vue
@@ -8,19 +8,19 @@
           <h4 class="text-md font-semibold">1.My Cart</h4>
         </div>
         <div  class="flex flex-col items-center w-full ">
-          <icon name="material-symbols:map" color="red" size="35" class="opacity-80"></icon>
+          <icon name="material-symbols:map" color="" size="35" class="opacity-80"></icon>
           <h4 class="text-md font-semibold">2.Shipping info</h4>
         </div>
         <div  class="flex flex-col items-center w-full">
-          <icon name="material-symbols:local-shipping-rounded" color="red" size="35" class="opacity-80"></icon>
+          <icon name="material-symbols:local-shipping-rounded" color="" size="35" class="opacity-80"></icon>
           <h4 class="text-md font-semibold">3.Delivery info</h4>
         </div>
         <div class="flex flex-col items-center w-full">
-          <icon name="material-symbols:credit-card-outline-sharp" color="red" size="35" class="opacity-80"></icon>
+          <icon name="material-symbols:credit-card-outline-sharp" color="" size="35" class="opacity-80"></icon>
           <h4 class="text-md font-semibold">4.Payment</h4>
         </div>
         <div class="flex flex-col items-center w-full">
-          <icon name="material-symbols:check-circle" color="red" size="35" class="opacity-80"></icon>
+          <icon name="material-symbols:check-circle" color="" size="35" class="opacity-80"></icon>
           <h4 class="text-md font-semibold">5.Confirmation</h4>
         </div>
       </div>
@@ -55,7 +55,7 @@
                     <el-input style="width: 100px" type="text" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1').replace(/^0[^.]/, '0');" v-model.number="addToCart.quantity"> </el-input>
                     <el-button type="primary" :icon="Plus" circle @click="increaseCartQty(addToCart.id)"/>
                   </div>
-                  <div class="text-md font-semibold primary">{{ addToCart.price * addToCart.quantity }}</div>
+<!--                  <div class="text-md font-semibold primary">{{addToCart.total= addToCart.price * addToCart.quantity }}</div>-->
                   <el-button :icon="Delete"  circle @click="removeCart(addToCart.id)"></el-button>
                 </div>
                </div>
@@ -87,17 +87,14 @@ import {
   Plus,
   Minus,
   Delete
-} from '@element-plus/icons-vue'
+} from '@element-plus/icons-vue';
 import { storeToRefs } from "pinia";
 import { useStores } from "~/store/store";
 import useUserBuyActivity from "~/composables/useUserBuyActivity";
 const {
   removeCart,
-  compareProduct,
   increaseCartQty,
   decreaseCartQty
 } = useUserBuyActivity();
 const { addToCarts } = storeToRefs(useStores());
-console.log(addToCarts)
-
 </script>

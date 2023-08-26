4fce7dbbec138958c6859d36e2294aff4b3bab6a
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-08-27 04:09:12 +0700

    create order-confirmed and payment-method page layout

---
+++ pages/checkout/order-confirmed.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <CheckoutProcess/>
+    <section class="mb-4" id="cart-summary">
+      <div class="w-full lg:container mx-auto px-4 py-6">
+        <div class="shadow bg-white py-3 p-lg-4 rounded ">
+          <div>THis is order confirmation page</div>
+        </div>
+        <div class="flex items-center py-6 justify-between ">
+          <div class="order-1 ">
+            <a href="/public" class="btn primary font-bold hover:underline">
+              <icon name="material-symbols:arrow-back-rounded"></icon>
+              Return to shop
+            </a>
+          </div>
+          <div class="order-2 block ">
+            <button class="btn bg-primary font-bold px-3 py-2 text-white rounded-md">Continue to
+              Shipping</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>

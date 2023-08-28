3997af0c8d8fb85b64f59dcea6f0b692b7647c91
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-08-28 20:29:41 +0700

    create layout for notification page

---
+++ pages/user-profile/all-notifications.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="w-full lg:container mx-auto py-6 bg-white ">
+    <div class="border-b pb-4 px-6">
+      <h3 class="text- lg font-semibold">Notifications</h3>
+    </div>
+    <div class="flex flex-col px-6">
+      <div class="flex flex-col  p-6">
+        <div class="flex mb-2 text-md">
+          <p>Your Order:</p>
+          <p><a class="primary" href="#">20230827-19284084 </a> has been Place</p>
+        </div>
+        <p class="text-sm">August 26 2023, 9:40 pm</p>
+      </div>
+      <div class="flex flex-col border-t p-6">
+        <div class="flex mb-2 text-md">
+          <p>Your Order:</p>
+          <p><a class="primary" href="#">20230827-19284084 </a> has been Place</p>
+        </div>
+        <p class="text-sm">August 26 2023, 9:40 pm</p>
+      </div>
+      <div class="flex flex-col border-t p-6">
+        <div class="flex mb-2 text-md">
+          <p>Your Order:</p>
+          <p><a class="primary" href="#">20230827-19284084 </a> has been Place</p>
+        </div>
+        <p class="text-sm">August 26 2023, 9:40 pm</p>
+      </div>
+      <div class="flex flex-col border-t p-6">
+        <div class="flex mb-2 text-md">
+          <p>Your Order:</p>
+          <p><a class="primary" href="#">20230827-19284084 </a> has been Place</p>
+        </div>
+        <p class="text-sm">August 26 2023, 9:40 pm</p>
+      </div>
+
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+
+</style>

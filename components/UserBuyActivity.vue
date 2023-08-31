a06eaf62ba23c8cca2c18769fe7df91dcba373e5
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-09-01 05:08:20 +0700

    change style

--- components/UserBuyActivity.vue
+++ components/UserBuyActivity.vue
@@ -1,14 +1,14 @@
 
 <template>
   <div class="absolute top-0 right-0 cartList-icon flex-col flex ">
-    <a href="#" class="bg-white rounded-full  mt-2 p-2" @click="addToWishlist(prop.Product)">
+    <a href="#" class="bg-white w-10 h-10 flex items-center justify-center mt-3 rounded-full   " @click="addToWishlist(prop.Product)">
       <Icon name="material-symbols:favorite-outline-rounded" size="20px" />
     </a>
-    <a href="#" class="bg-white rounded-full my-2 p-2" @click="compareProduct(prop.Product)">
+    <a href="#" class="bg-white w-10 h-10 flex items-center justify-center  rounded-full my-2 " @click="compareProduct(prop.Product)">
       <Icon name="material-symbols:sync-outline" size="20px" />
     </a>
-    <a href="#" class="bg-white rounded-full p-2" @click="addToCart(prop.Product)">
-      <Icon name="material-symbols:shopping-cart-outline" size="20px"/>
+    <a href="#" class="bg-white w-10 h-10 flex items-center justify-center  rounded-full " @click="addToCart(prop.Product)">
+      <Icon name="material-symbols:shopping-cart-outline" size="22px"/>
     </a>
   </div>
 </template>

cbc611154342e76937cb33e5ac7ab4b4a991c91a
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-09-01 05:01:43 +0700

    remove space

--- composables/useCategory.js
+++ composables/useCategory.js
@@ -3,23 +3,18 @@ import { useStores } from '~/store/store';
 import { storeToRefs } from "pinia";
 export default function useCategory() {
     const categories = ref([]);
-
-
-
     const { apiServer } = storeToRefs(useStores());
     const getCategory = async () => {
         await axios.get(apiServer.value + '/category?id=43')
             .then(response => {
                 categories.value = response.data.data.children;
-
             }).catch((error)=> {
-
+                console.log(error)
         });
     }
     getCategory();
 
     return {
         categories
-
     }
 }

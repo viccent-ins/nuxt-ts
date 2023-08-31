4bb1f5164c85cfa44766d07960bd5f22c32da108
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-09-01 05:12:50 +0700

    remove space and remove unused variable name relatedProductData

--- composables/useRelatedProduct.js
+++ composables/useRelatedProduct.js
@@ -9,12 +9,11 @@ export default function useRelatedProduct() {
     const route = useRoute();
     const getRelatedProduct = async () => {
         const cat_ids = route.query.cat_ids;
+
         isLoading.value = true;
         await axios.get(apiServer.value + `/goods/lists?category_id=${cat_ids}`)
             .then(response => {
                 relatedProducts.value = response.data.data.data;
-                const relatedProductData = relatedProducts.value
-
             }).catch((error) => {
                 console.log(error);
             });

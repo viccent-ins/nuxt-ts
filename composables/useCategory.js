feebf387fa066227f6f82bc9a43d2617aeffbf9f
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-09-03 01:32:32 +0700

    use isLoading

--- composables/useCategory.js
+++ composables/useCategory.js
@@ -4,17 +4,20 @@ import { storeToRefs } from "pinia";
 export default function useCategory() {
     const categories = ref([]);
     const { apiServer } = storeToRefs(useStores());
+    const isLoading = ref(false);
     const getCategory = async () => {
+        isLoading.value = true;
         await axios.get(apiServer.value + '/category?id=43')
             .then(response => {
                 categories.value = response.data.data.children;
             }).catch((error)=> {
                 console.log(error)
         });
+        isLoading.value = false;
     }
     getCategory();
-
     return {
-        categories
+        categories,
+        isLoading
     }
 }

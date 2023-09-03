3056365377d704bd1d14fef4160c2284b2bb46d1
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-09-03 14:49:30 +0700

    use input tag from registration form

--- pages/register.vue
+++ pages/register.vue
@@ -7,23 +7,17 @@
             Create an account
           </h1>
         </div>
-        <el-form label-position="top" class="p-6">
-          <el-form-item >
-            <el-input size="large" placeholder="Phone" v-model="registerRequest.mobile" type="tel" autocomplete="off" />
-          </el-form-item>
-          <el-form-item >
-            <el-input size="large" placeholder="Password" v-model="registerRequest.password" type="password" autocomplete="off" />
-          </el-form-item>
-          <el-form-item >
-            <el-input size="large" placeholder="Code" v-model="registerRequest.code" type="text" autocomplete="off" />
-          </el-form-item>
-            <el-checkbox v-model="checked" label="By Signing up you agree to our terms and conditions" size="large" />
-
-          <el-form-item >
-            <el-button class="w-full" size="large" type="primary" @click="register" :loading="isProcessing"
-            >Submit</el-button>
-          </el-form-item>
-        </el-form>
+        <form  class="p-6" >
+          <input class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm" placeholder="Phone" v-model.number="registerRequest.mobile" type="tel" required >
+          <input class="my-5 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm" type="password" placeholder="Password" v-model="registerRequest.password" required>
+          <input class="mb-5 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm" type="text"  placeholder="Code" v-model="registerRequest.code">
+          <div class="flex gap-3 mb-5">
+            <input v-model="checked" type="checkbox" id="checkConditions" >
+            <label for="checkConditions">I have read and complied with</label>
+            <nuxt-link class="primary hover:underline"  to="/terms">"User Agreement" and "Privacy Agreement"</nuxt-link>
+          </div>
+          <button class="w-full btn bg-primary rounded-md py-2 text-white text-lg" @click="register" :loading="isProcessing">Create Account</button>
+        </form>
         <div class="text-center mt-4 mb-6">
           <p class="text-muted mb-0">Already have an account?</p>
           <NuxtLink to="/login" class="primary">Log In</NuxtLink>
@@ -38,18 +32,20 @@ import { useStores } from "~/store/store";
 import { storeToRefs } from "pinia";
 const checked = ref(false);
 const stores = useStores();
-
 const { isAuth } = storeToRefs(stores);
 const {
   registerRequest,
   isProcessing,
-    register
+    register,
+
 } = useRegister();
 const authMiddle = () => {
   if (isAuth.value) {
     navigateTo('/');
+
   }
 }
 onMounted(() => authMiddle());
+
 </script>
 

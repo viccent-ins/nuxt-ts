d06479f11f7d8259ec270777a1a96b347ad6eb3c
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-09-03 14:41:10 +0700

    change file path name

--- components/CompanyPolicy.vue
+++ components/CompanyPolicy.vue
@@ -6,9 +6,9 @@
           <icon name="material-symbols:help" color="red" size="35" class="opacity-80"></icon>
           <h4 class="text-md font-semibold">Frequently Asked Questions</h4>
         </nuxt-link>
-        <nuxt-link to="/return-policy" class="flex flex-col items-center w-full border-l-2 py-10">
-          <icon name="ic:baseline-reply" color="red" size="35" class="opacity-80"></icon>
-          <h4 class="text-md font-semibold">return policy</h4>
+        <nuxt-link to="/user-agreement" class="flex flex-col items-center w-full border-l-2 py-10">
+          <icon name="icon-park-solid:agreement" color="red" size="35" class="opacity-80"></icon>
+          <h4 class="text-md font-semibold">User Agreement</h4>
         </nuxt-link>
         <nuxt-link to="/about-company" class="flex flex-col items-center w-full border-l-2 py-10">
           <icon name="ic:baseline-support" color="red" size="35" class="opacity-80"></icon>

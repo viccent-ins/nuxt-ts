dd081ec44b67524e999fd2734b92cdba68bebf5f
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-09-04 03:55:33 +0700

    add form

--- pages/register.vue
+++ pages/register.vue
@@ -8,8 +8,8 @@
           </h1>
         </div>
         <form  class="p-6" >
-          <input class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm" placeholder="Phone" v-model.number="registerRequest.mobile" type="tel" required >
-          <input class="my-5 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm" type="password" placeholder="Password" v-model="registerRequest.password" required>
+          <input class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm" placeholder="Phone" autocomplete="off" v-model.number="registerRequest.mobile" type="tel" required >
+          <input class="my-5 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm" type="password" autocomplete="off" placeholder="Password" v-model="registerRequest.password" required>
           <input class="mb-5 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm" type="text"  placeholder="Code" v-model="registerRequest.code">
           <div class="flex mb-5">
             <input v-model="checked" type="checkbox" id="checkConditions" >
